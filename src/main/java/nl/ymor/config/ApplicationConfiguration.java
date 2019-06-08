59d00af59f7d2899732b45c572e11204a3e644f3
Cem Kilickaplan <ckilickaplan@ymor.com>
2019-06-08 16:19:16 +0200

    Explicitly ordered the bean creation. Otherwise when the RestClient is created the arguments may not be set in there.

--- src/main/java/nl/ymor/config/ApplicationConfiguration.java
+++ src/main/java/nl/ymor/config/ApplicationConfiguration.java
@@ -6,26 +6,31 @@ import nl.ymor.service.BackupService;
 import nl.ymor.service.RestClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
 @Configuration
 public class ApplicationConfiguration {
 
     @Bean
+    @Order(10)
     public Arguments arguments() {
         return new Arguments();
     }
 
     @Bean
+    @Order(30)
     public RestClient restClient(Arguments arguments) {
         return new RestClient(arguments);
     }
 
     @Bean
+    @Order(20)
     public BackupService backupService(Arguments arguments, RestClient restClient) {
         return new BackupService(arguments, restClient);
     }
 
     @Bean
+    @Order(40)
     public JiraBackup jiraBackup(BackupService backupService) {
         return new JiraBackup(backupService);
     }
